8cf828ab32fd5098400aaa01bcfcd42b7902b061
sazid-hassan <sazid.khan.hassan@gmail.com>
2021-02-17 19:50:42 +0600

    fixed auto click issues

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
+
 const getID = id => document.getElementById(id);
 
-//var heading = getID('post-head').value;
-//var content = getID('post-content').value;
-
-getID('full-post').style.display = 'none';
 getID('card-post').style.display = 'none';
+getID('full-post').style.display = 'none';
 
-getID('submit').addEventListener('click', ()=> {
-    const heading = getID('post-head').value;
+
+
+
+getID('submit').addEventListener('click', ()=>{
+    const head = getID('post-head').value;
+    // console.log(head);
     const content = getID('post-content').value;
-    getID('card-head').innerText = heading;
-    getID('card-content').innerText = content.slice(0,150)+'...';
+    // console.log(content);
 
-
-    //Making disappear the input box
+    getID('post-input').style.display = 'none';
     getID('card-post').style.display = 'block';
 
-    getID('input-form').style.display='none';
 
+    getID('card-head').innerText = head;
+    getID('card-content').innerText = content.slice(0,150)+'...';
 })
-
 
 getID('switcher').addEventListener('click', ()=>{
-    getID('full-post').style.display = 'block';
     getID('card-post').style.display = 'none';
-    getID('full-post-heading').innerText = getID('post-head').value;
-    console.log(getID('post-head').value)
-    getID('full-post-content').innerText = getID('post-content').value;
-    console.log(getID('post-content').value)
+    getID('full-post').style.display = 'block';
+    getID('full-heading').innerText = getID('post-head').value;
+    console.log(getID('post-head').value);
+    getID('full-content').innerText = getID('post-content').value;
 
 })
-
